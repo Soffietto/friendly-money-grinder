8fde127aeea296470de0fef49153aaeb26a75ad9
Azat khayrullin <azot-1997@yandex.ru>
2017-07-28 16:45:59 +0300

    now resources are random

--- src/main/java/ru/jblab/friendlymoney/config/WebConfig.java
+++ src/main/java/ru/jblab/friendlymoney/config/WebConfig.java
@@ -1,33 +1,39 @@
-//package ru.jblab.friendlymoney.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
-//import org.springframework.core.env.Environment;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebMvc
-//@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
-//public class WebConfig extends WebMvcConfigurerAdapter {
-//
-//    @Autowired
-//    private Environment env;
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-////        String staticPath = env.getProperty("spring.resources.static-locations") + "**";
-//        registry.addResourceHandler("/css/**", "/js/**").
-//                addResourceLocations(
-//                        "classpath:/static/css/",
-//                        "classpath:/static/js/");
-//        registry.addResourceHandler("/fonts/**", "/images/**").
-//                addResourceLocations(
-//                        "classpath:/static/fonts/",
-//                        "classpath:/static/images/");
-//    }
-//    //TODO Исправить
-//}
+package ru.jblab.friendlymoney.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+@Configuration
+@EnableWebMvc
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    private final Integer DIRECTORY_COUNTER = 2;
+    private int randomDirectory = (int) (Math.random() * DIRECTORY_COUNTER) + 1;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**", "/js/**", "/fonts/**", "/images/**").
+                addResourceLocations(
+                        "classpath:makets/maket" + randomDirectory + "/static/css/",
+                        "classpath:makets/maket" + randomDirectory + "/static/js/",
+                        "classpath:makets/maket" + randomDirectory + "/static/fonts/",
+                        "classpath:makets/maket" + randomDirectory + "/static/images/"
+                        );
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("classpath:makets/maket" + randomDirectory + "/templates/");
+        return freeMarkerConfigurer;
+    }
+}
