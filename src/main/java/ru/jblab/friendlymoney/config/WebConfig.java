dbce53b6e1320e38a1641622b1c3a56d049036f1
Azat khayrullin <azot-1997@yandex.ru>
2017-07-28 17:11:30 +0300

    now java counts dirrectories in maket package

--- src/main/java/ru/jblab/friendlymoney/config/WebConfig.java
+++ src/main/java/ru/jblab/friendlymoney/config/WebConfig.java
@@ -6,18 +6,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
 @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    private final Integer DIRECTORY_COUNTER = 2;
-    private int randomDirectory = (int) (Math.random() * DIRECTORY_COUNTER) + 1;
+    private int randomDirectory = (int) (Math.random() * countDirectories()) + 1;
+
+
+    private int countDirectories() {
+        int directoryCounter = 0;
+        try {
+            directoryCounter = new ClassPathResource("makets").getFile().listFiles().length;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return directoryCounter;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
